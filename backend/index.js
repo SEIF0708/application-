480dd4d999d937b57096cdbf410f615f0e6eae55
SEIF0708 <saifdaba19@gmail.com>
2025-07-14 23:27:18 +0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://userinterface-vbpi.onrender.com',
+    credentials: true
+}));
 app.use(express.json());
 
 // MongoDB connection
